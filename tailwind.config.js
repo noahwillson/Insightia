94f29b0852d88bbf1d467136c8ab9ab4759f4940
noahwillson <nwillson2601@gmail.com>
2024-03-10 03:14:18 +0200

    Tailwind: added color object

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primary: "#004D7F",
+      secondary: "#00A3AF",
+      tertiary: "#F5F5F5",
+      quaternary: "#333333",
+      quinary: "#FFCC00",
+    },
   },
   plugins: [],
 };
